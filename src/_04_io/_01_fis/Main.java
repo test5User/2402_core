c8faf4da2560d09250531c786ec5aa4db1e5acb0
Test User <Test5User@yandex.by>
2024-10-18 21:11:07 +0300

    Input-output second

--- src/_04_io/_01_fis/Main.java
+++ src/_04_io/_01_fis/Main.java
@@ -1,4 +1,29 @@
 package _04_io._01_fis;
 
+import lombok.SneakyThrows;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
 public class Main {
+
+
+    public static final String TEST_EXT = "src/_04_io/_01_fis/test.ext";
+    public static final String TEST_1_EXT = "src/_04_io/_01_fis/test1.ext";
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        try (var fos = new FileOutputStream(TEST_EXT);
+             var fis = new FileInputStream(TEST_EXT);
+             var fos1 = new FileOutputStream(TEST_1_EXT)){
+            var outByteArray = "Hello мир!!!".getBytes();
+            fos.write(outByteArray);
+
+            byte[] bytes = fis.readAllBytes();
+            System.out.println(new String(bytes));
+            fos1.write(bytes);
+        }
+    }
 }
